bff079ccc358cffe9d6981c835addd155cbe9146
dmtrby <dmitry.mahliui@gmail.com>
2019-08-17 12:52:23 +0300

    Updates

---
+++ precache-manifest.37fccaa6075e8962f33f39bead8b12f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02d84b4a51a52b355495052aa52b6c8",
+    "url": "/webchat/index.html"
+  },
+  {
+    "revision": "593f49fa0a7c9ba125a0",
+    "url": "/webchat/static/css/main.1b784748.chunk.css"
+  },
+  {
+    "revision": "256b4fc93b1ba92a80e1",
+    "url": "/webchat/static/js/2.65a8602d.chunk.js"
+  },
+  {
+    "revision": "593f49fa0a7c9ba125a0",
+    "url": "/webchat/static/js/main.65bd6939.chunk.js"
+  },
+  {
+    "revision": "f293ce9384a2569b7364",
+    "url": "/webchat/static/js/runtime~main.3a621833.js"
+  }
+]);
